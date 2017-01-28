cd2ab69a8478541f7bf11ca76cf14c1f654f0af5
rud9321 <rud9321@gmail.com>
2017-01-28 12:53:09 +0530

    success

---
+++ WebAPI/Controllers/RudController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using WebAPI.Repository;
+
+namespace WebAPI.Controllers
+{
+    public class RudController : ApiController
+    {
+        [HttpGet]
+        public List<MsCodeModel> GetAll()
+        {
+            return MsCodeRepository.GetAll();
+        }
+        [HttpGet]
+        public MsCodeModel Get(int id)
+        {
+            return MsCodeRepository.GetOne(id);
+        }
+        [HttpPost]
+        public IHttpActionResult Post(MsCodeModel model)
+        {
+            MsCodeRepository.GetAll().Add(model);
+            return CreatedAtRoute("DefaultApi", new { id = model.Id }, model);
+        }
+        [HttpPut]
+        public IHttpActionResult Put(int id, MsCodeModel model)
+        {
+            MsCodeRepository.UpdateData(id, model);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            MsCodeRepository.DeleteData(id);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ WebAPI/Repository/MsCodeRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+
+namespace WebAPI.Repository
+{
+    public class MsCodeRepository
+    {
+        public static List<MsCodeModel> GetAll()
+        {
+            List<MsCodeModel> modellist = new List<MsCodeModel>()
+            {
+                new MsCodeModel{
+                    Id=1001,
+                    Name = "Rudra",
+                    Details ="Software Developer"
+            },
+            new MsCodeModel{
+                    Id=1002,
+                    Name = "Sonu Singh",
+                    Details ="Software Developer"
+            }
+            };
+            return modellist;
+        }
+        public static MsCodeModel GetOne(int id)
+        {
+            return MsCodeRepository.GetAll().Where(x => x.Id.Equals(id)).SingleOrDefault();
+        }
+        public static void UpdateData(int id, MsCodeModel model)
+        {
+            var data = MsCodeRepository.GetAll().Where(x => x.Id.Equals(id)).SingleOrDefault();
+            data.Name = model.Name;
+            data.Details = model.Details;
+            //do update on server
+        }
+        public static MsCodeModel DeleteData(int id)
+        {
+            return MsCodeRepository.GetAll().Where(x => x.Id.Equals(id)).SingleOrDefault();
+        }
+    }
+}
